852fc8adfde5699b57ce68b80527957b6f85b72a
salvdor <cuyca11@gmail.com>
2020-01-22 18:35:35 +0100

    22/01/2020
    primera version 1.1
    el archivo T1E1 se ha modificado

--- src/boletin1/T1E1.java
+++ src/boletin1/T1E1.java
@@ -6,7 +6,7 @@ Escribe una programa que muestre tu nombre por pantalla.
  */
 public class T1E1 { // Clase principal
 public static void main(String[] args) {
-System.out.println("Salvador");
+System.out.println("Salvador Oliva");
 
   }
 }
