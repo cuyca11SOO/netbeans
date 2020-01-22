f630a7b4b3bb9c00933e0093eb7fb12e78b7c0a1
salvdor <cuyca11@gmail.com>
2020-01-22 19:40:57 +0100

    22/01/2020
    primera version 1.1
    el archivo T1E1 se ha modificado

--- src/boletin1/T1E2.java
+++ src/boletin1/T1E2.java
@@ -3,12 +3,12 @@ package boletin1;
 /*
  Ejercicio 2
 Modifica el programa anterior para que además se muestre tu dirección y
-tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+tu numero de telofono. Asegúrate de que los datos se muestran en líneas
 separadas.
  */
 public class T1E2 { // Clase principal
 public static void main(String[] args) {
-System.out.println("Nombre: Salvador");
+System.out.println("Nombre: Salvador Oliva");
 System.out.println("Direcion: Paseo del Parque,12");
 System.out.println("Telefono: 123456789");
   }
